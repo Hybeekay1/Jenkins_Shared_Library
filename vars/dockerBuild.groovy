aced2b22368180c91283af95ab9ee38f0610a5c5
Hybeekay1 <68868730+Hybeekay1@users.noreply.github.com>
2023-10-17 10:48:02 +0100

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,4 +1,4 @@
-def call(String project, String ImageTag, String hubUser){
+def call(String project, String hubuser){
        sh """
         docker image build -t ${hubuser}/${project} . }
         docker image tag ${hubuser}/${project} ${hubuser}/${project}:latest
